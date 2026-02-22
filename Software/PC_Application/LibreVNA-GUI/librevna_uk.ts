54a3d66dc270a887d8f506ce4b68bfb2bdf3f7a8
RomanMordzynsky <104225304+RomanMordzynsky@users.noreply.github.com>
2026-02-22 10:43:00 +0200

    Add files via upload

---
+++ Software/PC_Application/LibreVNA-GUI/librevna_uk.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk_UA">
+<context>
+    <name>AppWindow</name>
+    <message>
+        <source>&amp;Language</source>
+        <translation>&amp;Мова</translation>
+    </message>
+    <message>
+        <source>English</source>
+        <translation>Англійська</translation>
+    </message>
+    <message>
+        <source>Ukrainian</source>
+        <translation>Українська</translation>
+    </message>
+</context>
+</TS>
